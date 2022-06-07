b7c26781ad5a9ac8a0f532f993d592c991713fad
Dmitry Voskoboynikov <dmitry.voskoboynikoff@gmail.com>
2022-06-07 09:01:35 +0300

    simple commit

--- chapter1/index.js
+++ chapter1/index.js
@@ -1,1 +1,16 @@
+console.log("Ben & Jerry's Chocolate Fudge Brownie");
+
+console.log("Ben & Jerry's" + " " + "Chocolate Fudge Brownie" + " is my favorite ice cream.");
+
+console.log(4 * 260);
+
+console.log((6 * 14 + 21) / 7 * 365);
+
+console.log( Math.round((6 * 14 + 21) / 7 * 365 * 100 / (4 * 260)) + " pints of Chocolate Fudge Brownie");
+
+console.log( "Chocolate Fudge Brownie" === "chocolate ice cream" );
+
+console.log( Math.round((6 * 14 + 21) / 7 * 365 * 100 / (4 * 260)) + " pints of Chocolate Fudge Brownie" === "526 pints of Chocolate Fudge Brownie" );
+
+
 
