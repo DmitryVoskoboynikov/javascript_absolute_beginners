60fbe833b37d7e93432e1a4ff9bf0bf23a688790
Dmitry Voskoboynikov <dmitry.voskoboynikoff@gmail.com>
2022-06-08 21:23:30 +0300

    simple commit

--- chapter2_Type_Conversion/index.js
+++ chapter2_Type_Conversion/index.js
@@ -14,5 +14,34 @@ console.log(id.charCodeAt(10) === 252);
 console.log(id.charAt(10) === "ü");
 
 //Converting Case
+console.log("Pow! Bam! Zonk!".toUpperCase());
+console.log("PSST...ZZZZ".toLowerCase());
 
+console.log("I".toLowerCase());
+console.log("i".toUpperCase());
 
+//console.log("I".toLocaleLowerCase());
+//console.log("i".toLocaleUpperCase());
+
+//Locating a Substring
+console.log("Iron Monger, Titanium Man, Madame Masque, Ghost, Mandarin".indexOf("Ghost"));
+
+var villains = "Iron Monger, Titanium Man, Madame Masque, Ghost, Mandarin";
+console.log(villains.indexOf("Mandarin"));
+console.log(villains.indexOf("Green Goblin"));
+
+console.log(villains.indexOf("Man"));
+console.log(villains.indexOf("Man", villains.indexOf("Man") + 1));
+
+//Clipping a Substring
+console.log("Superman, Batman, Spider-Man, Iron Man".slice(18, 24));
+
+//Replacing a Substring
+console.log("Batman and Batgirl".replace(/Bat/g, "Super"));
+
+var titleCase = function(m) {
+    return m.slice(0,1).toUpperCase() + m.slice(1);
+};
+console.log("batman, spider-man, iron man".replace(/\b\w+\b/g, titleCase));
+
+//Splitting a String into an Array of Smaller Strings
