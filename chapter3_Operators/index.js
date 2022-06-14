388ff243b2e9dcc4ee5bda36a29757868379767f
Dmitry Voskoboynikov <dmitry.voskoboynikoff@gmail.com>
2022-06-14 20:31:18 +0300

    simple commit

--- chapter3_Operators/index.js
+++ chapter3_Operators/index.js
@@ -184,4 +184,107 @@ var compare = "apple" > "Granny Smith";
 console.log(compare); //true
 console.log("apple".toLowerCase() > "Granny Smith".toLowerCase()); // false
 
+// Determining Whether One Number or String Is Less Than Another
+console.log("/** Determining Whether One Number or String Is Less Than Another */");
+
+var cake = {
+    dough: {
+        organicPastryFlour: [1 + 1 / 2, "cup"],
+        freshlyGroundNutmeg: [1 / 4, "tsp"],
+        saigonCinnamon: [1/2, "tsp"],
+        soda: [1, "tsp"],
+        tartar: [1, "tsp"],
+        egg: [2],
+        yogurtCheese: [2/3, "cup"],
+        pureMapleSyrup: [1/3, "cup"],
+        shreddedGrannySmith: [1 + 2/3, "cup"],
+        choppedPecans: [1/2, "cup"]
+    },
+    icing: {
+        yogurtCheese: [2/3, "cup"],
+        pureMapleSyrup: [1 + 1/3, "tbs"],
+        groundPecans: [2, "tsp"]
+    }
+}
+
+console.log(cake);
+
+console.log(cake.dough.organicPastryFlour[0] < cake.dough.shreddedGrannySmith[0]); //true
+console.log(cake.dough.choppedPecans[0] < cake.dough.pureMapleSyrup[0]); //false
+console.log(cake.dough.freshlyGroundNutmeg[0] < cake.dough.saigonCinnamon[0]); //true
+console.log(cake.icing.yogurtCheese[0] < cake.dough.yogurtCheese[0]); //false
+
+// Greater Than or Equal to, Less Than or Equal to
+console.log("/** Greater Than or Equal to, Less Than or Equal to */");
+
+var cake = {
+    dough: {
+        organicPastryFlour: [1 + 1/2, "cup"],
+        freshlyGroundNutmeg: [1/4, "tsp"],
+        saigonCinnamon: [1/2, "tsp"],
+        soda: [1, "tsp"],
+        tartar: [1, "tsp"],
+        egg: [2],
+        yogurtCheese: [2/3, "cup"],
+        pureMapleSyrup: [1/3, "cup"],
+        shreddedGrannySmith: [1 + 2/3, "cup"],
+        choppedPecans: [1/2, "cup"]
+    },
+    icing: {
+        yogurtCheese: [2/3, "cup"],
+        pureMapleSyrup: [1 + 1/3, "tbs"],
+        groundPecans: [2, "tsp"]
+    }
+};
+
+console.log(cake.icing.yogurtCheese[0] <= cake.dough.yogurtCheese[0]); //true
+console.log(cake.icing.yogurtCheese[0] >= cake.dough.yogurtCheese[0]); //true
+console.log(cake.dough.organicPastryFlour[0] <= cake.dough.shreddedGrannySmith[0]); //true
+console.log(cake.dough.choppedPecans[0] >= cake.dough.pureMapleSyrup[0]); //true
+
+// Creating More Complex Comparisons
+console.log("/** Creating More Complex Comparisons */");
+
+// Saying or With ||
+console.log("/** Saying or With || */");
+
+var muffin = {
+    oatFlour: [1/3, "cup"],
+    barleyFlour: [1/3, "cup"],
+    pastryFlour: [1 + 1/3, "cup"],
+    freshlyGroundNutmeg: [1/4, "tsp"],
+    saigonCinnamon: [1/2, "tsp"],
+    seaSalt: [1/4, "tsp"],
+    soda: [1, "tsp"],
+    tartar: [1, "tsp"],
+    mapleBrownCow: [1 + 1/2, "cup"],
+    boysenberries: [2, "cup"],
+    choppedPecans: [1/3, "cup"]
+};
+
+console.log(muffin.mapleBrownCow[0] > muffin.boysenberries[0] || muffin.oatFlour[0] === muffin.barleyFlour[0]); //true
+console.log(muffin.boysenberries[0] < muffin.choppedPecans[0] || muffin.pastryFlour[0] < muffin.barleyFlour[0]); //false
+
+// Saying "and" with &&
+console.log("/** Saying \"and\" with && */");
+
+var muffin = {
+    oatFlour: [1/3, "cup"],
+    barleyFlour: [1/3, "cup"],
+    pastryFlour: [1 + 1/3, "cup"],
+    freshlyGroundNutmeg: [1/4, "tsp"],
+    saigonCinnamon: [1/2, "tsp"],
+    seaSalt: [1/4, "tsp"],
+    soda: [1, "tsp"],
+    tartar: [1, "tsp"],
+    mapleBrownCow: [1 + 1/2, "cup"],
+    boysenberries: [2, "cup"],
+    choppedPecans: [1/3, "cup"]
+};
+
+console.log(muffin.mapleBrownCow[0] > muffin.boysenberries[0] && muffin.oatFlour[0] === muffin.barleyFlour[0]); //false
+console.log(muffin.boysenberries[0] > muffin.choppedPecans[0] && muffin.pastryFlour[0] > muffin.barleyFlour[0]); //true
+
+// Conditionally Returning One of Two Values
+console.log("/** Conditionally Returning One of Two Values */");
 
