76698eec95b081cd846d0f3dfc1c813d62449e16
Dmitry Voskoboynikov <dmitry.voskoboynikoff@gmail.com>
2022-06-15 12:56:42 +0300

    simple commit

--- chapter4_Controlling_Flow/index.js
+++ chapter4_Controlling_Flow/index.js
@@ -33,6 +33,7 @@ console.log(mayfly.addToTally(110));
 
 console.log(mayfly.kmLeftToLive());
 
+
 // Coding Several Paths with the else if Idiom
 console.log("/** Coding Several Paths with the else if Idiom */");
 
@@ -71,3 +72,147 @@ var smoothie = fridge.brownCow ? "Brown Cow cream-top yogurt" :
         (fridge.fage ? "Fage cultured cream" :
             (fridge.lifeway ? "Lifeway Greek-style kefir" : "Dannon yogurt")));
 console.log(smoothie += ", grass-fed cream and milk, Saigon cinnamon, and wild blueberries.");
+
+// Taking One of Several Paths with a Switch
+console.log("/** Taking One of Several Paths with a Switch */");
+
+var jersey = 92, name = "";
+
+switch (jersey) {
+    case 7:
+        name = "Roethlisberger";
+        break;
+    case 10:
+        name = "Holmes";
+        break;
+    case 17:
+        name = "Wallace";
+        break;
+    case 34:
+        name = "Mendenhall";
+        break;
+    case 43:
+        name = "Polamalu";
+        break;
+    case 83:
+        name = "Miller";
+        break;
+    case 92:
+    case 97:
+        name = "Harrison";
+        break;
+    case 94:
+        name = "Timmons";
+        break;
+    case 96:
+        name = "Hood";
+        break;
+    default:
+        name = "not worn by any Steeler";
+        break;
+}
+
+console.log("Number " + jersey + " is " + name + ".");
+
+var jersey = 7, name = "";
+
+function identifyPlayer() {
+    switch (jersey) {
+        case 7:
+            return "Roethlisberger";
+        case 10:
+            return "Holmes";
+        case 17:
+            return "Wallace";
+        case 34:
+            return "Mendenhall";
+        case 43:
+            return "Polamalu";
+        case 83:
+            return "Miller";
+        case 86:
+            return "Ward";
+        case 92:
+            return "Harrison";
+        case 94:
+            return "Timmons";
+        case 96:
+            return "Hood";
+        default:
+            return "not worn by any Steeler";
+    }
+}
+
+console.log("Number " + jersey + " is " + identifyPlayer() + ".");
+
+// Writing a while Loop
+console.log("/** Writing a while Loop */");
+
+var looseLeafTea = [
+    "Ghillidary",
+    "Kenilworth",
+    "Milima",
+    "Keemun",
+    "Boisahabi",
+    "Manohari",
+    "Borpatra",
+    "Lukwah",
+    "Khongea"
+];
+
+var mood = "glum";
+var i = 0;
+
+while (i < looseLeafTea.length) {
+    if (looseLeafTea[i] === "Borpatra") {
+        mood = "chery";
+        break;
+    }
+
+    i++;
+}
+
+console.log("I feel " + mood + "!");
+
+// Aborting an Iteration but Not the Loop
+console.log("/** Aborting an Iteration but Not the Loop */");
+
+var looseLeafTea = [
+    "Ghillidary",
+    "",
+    "Kenilworth",
+    "Milima",
+    ,
+    "Keemun",
+    "Boisahabi",
+    "Manohari",
+    "Borpatra",
+    "Lukwah",
+    "Khongea"
+];
+
+console.log(looseLeafTea);
+
+var mood = "glum";
+var i = 0;
+while (i < looseLeafTea.length) {
+    if (looseLeafTea[i] === "Borpatra") {
+        mood = "cheery";
+        break;
+    } else if (! looseLeafTea[i]) {
+        looseLeafTea.splice(i, 1);
+        continue;
+    } else {
+        i++;
+    }
+}
+
+console.log("I feel " + mood + "!");
+console.log(looseLeafTea);
+
+// Replacing Break with Return in a Function
+console.log("/** Replacing Break with Return in a Function */");
+
+
+
+
